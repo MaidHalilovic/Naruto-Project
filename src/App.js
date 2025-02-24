b4a556aabc9be3192f6ec24d5b7218e8c62e6b91
MaidHalilovic <maidhaliloviclimunada567@gmail.com>
2025-02-24 23:01:23 +0100

    Making header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import HomePage from "./pages/HomePage/HomePage";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className='App'>
       <BrowserRouter>
+        <Header />
         <Routes>
-          <Route />
+          <Route path='/' element={<HomePage />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className='main-Container'>
+      <div>nesto</div>
+    </div>
+  );
+};
+
+export default Header;
