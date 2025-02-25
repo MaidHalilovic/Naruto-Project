2392d5f432ec0957f244787448fc6c4adea43d77
MaidHalilovic <maidhaliloviclimunada567@gmail.com>
2025-02-25 20:50:26 +0100

    Made buttons to navigate to every page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
   return (
     <div className='main-Container'>
-      <div>nesto</div>
+      <div>
+        <button onClick={() => navigate("/AllCharacters")}>
+          AllCharacters
+        </button>
+        <button onClick={() => navigate("/Clans")}>Clans</button>
+        <button onClick={() => navigate("/Kekkeigenkai")}>Kekkeigenkai</button>
+        <button onClick={() => navigate("/Teams")}>Teams</button>
+        <button onClick={() => navigate("/Kara")}>Kara</button>
+        <button onClick={() => navigate("/TailedBeasts")}>TailedBeasts</button>
+        <button onClick={() => navigate("/Villages")}>Villages</button>
+        <button onClick={() => navigate("/Akatsuki")}>Akatsuki</button>
+      </div>
     </div>
   );
 };
